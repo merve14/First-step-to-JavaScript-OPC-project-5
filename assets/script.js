c2817139b12589cefd04674a0fa80f53e8926252
merve14 <merve.cetinkaya416@gmail.com>
2023-06-14 15:22:26 +0200

    20%

--- assets/script.js
+++ assets/script.js
@@ -18,12 +18,11 @@ const slides = [
   },
 ];
 
-
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
 
 arrowLeft.addEventListener("click", previous);
-arrowRight.addEventListener('click', next);
+arrowRight.addEventListener("click", next);
 
 const bannerImage = document.querySelector(".banner-img");
 bannerImage.src = "./assets/images/slideshow/" + slides[0].image;
@@ -31,52 +30,60 @@ bannerImage.src = "./assets/images/slideshow/" + slides[0].image;
 // je selectionne ma div .dots pour y inserer des bulles
 const containerDots = document.querySelector(".dots");
 
+// Je séléctionne le paragraphe sur l'image
 const parag = document.querySelector("#banner p");
 parag.innertext = slides[0].tagLine;
 
 // je boucle sur mes slides et a chaque fois je crée un point
 for (let i = 0; i < slides.length; i++) {
-	const point = document.createElement("div");
-	containerDots.appendChild(point);
-	point.classList.add("dot");	
+  const point = document.createElement("div");
+  containerDots.appendChild(point);
+  point.classList.add("dot");
 }
 
 const bullets = document.querySelectorAll(".dot");
-bullets[0].classList.add("dot_selected");	
-
+bullets[0].classList.add("dot_selected");
 
 // ma function pour passer à l'image précédente
-function previous() {   
-	// je definie l'index par defaut à zero       
-	let index = 0;
-	// je boucle pour voir si l'une des dots est selectionné
-	for (let i = 0; i < slides.length; i++) {
-		const test = bullets[i].classList;
-		// si je trouve que l'une des dots est selectionné alors je garde son index
-		if (test.contains("dot_selected")) {
-			index=i;
-		};	
-	}
-	// je supprime la class dot selected à la dot de l'index que j'ai trouvé pour pouvoir mettre à la dot précédente
-	bullets[index].classList.remove("dot_selected");
-	// si index est égal à 0 sa veut dire que nous somme sur la 1er slide alors on part sur la dernier slide
-	if (index==0) { index= slides.length - 1; } else { index--;};
-	bullets[index].classList.add("dot_selected");	
-	bannerImage.src = "./assets/images/slideshow/" + slides[index].image;
-	parag.innerHTML = slides[index].tagLine;
-};
+function previous() {
+  // je definie l'index par defaut à zero
+  let index = 0;
+  // je boucle pour voir si l'une des dots est selectionné
+  for (let i = 0; i < slides.length; i++) {
+    const test = bullets[i].classList;
+    // si je trouve que l'une des dots est selectionné alors je garde son index
+    if (test.contains("dot_selected")) {
+      index = i;
+    }
+  }
+  // je supprime la class dot selected à la dot de l'index que j'ai trouvé pour pouvoir mettre à la dot précédente
+  bullets[index].classList.remove("dot_selected");
+  // si index est égal à 0 sa veut dire que nous somme sur la 1er slide alors on part sur la dernier slide
+  if (index == 0) {
+    index = slides.length - 1;
+  } else {
+    index--;
+  }
+  bullets[index].classList.add("dot_selected");
+  bannerImage.src = "./assets/images/slideshow/" + slides[index].image;
+  parag.innerHTML = slides[index].tagLine;
+}
 
 function next() {
-	index = 0;
-	for (let i = 0; i < slides.length; i++) {
-		let test = bullets[i].classList;
-		if (test.contains("dot_selected")) {
-			index=i;
-		};	
-	}
-	bullets[index].classList.remove("dot_selected");
-	if (index==slides.length - 1) { index=0; } else { index++;};
-	bullets[index].classList.add("dot_selected");
-	bannerImage.src = "./assets/images/slideshow/" + slides[index].image;
-	parag.innerHTML = slides[index].tagLine;
-};
+  index = 0;
+  for (let i = 0; i < slides.length; i++) {
+    let test = bullets[i].classList;
+    if (test.contains("dot_selected")) {
+      index = i;
+    }
+  }
+  bullets[index].classList.remove("dot_selected");
+  if (index == slides.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  bullets[index].classList.add("dot_selected");
+  bannerImage.src = "./assets/images/slideshow/" + slides[index].image;
+  parag.innerHTML = slides[index].tagLine;
+}
